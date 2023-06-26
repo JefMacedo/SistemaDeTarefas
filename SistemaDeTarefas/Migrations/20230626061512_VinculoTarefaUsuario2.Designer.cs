bdcdd7104f6273176a85504bc848c8ff9891f716
Jeferson Macedo <jhef.salles@gmail.com>
2023-06-26 14:06:02 -0300

    Finalização do crud de tarefas

--- SistemaDeTarefas/Migrations/20230626061512_VinculoTarefaUsuario2.Designer.cs
+++ SistemaDeTarefas/Migrations/20230626061512_VinculoTarefaUsuario2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaDeTarefas.Data;
 
@@ -11,9 +12,11 @@ using SistemaDeTarefas.Data;
 namespace SistemaDeTarefas.Migrations
 {
     [DbContext(typeof(SistemaDeTarefasDBContext))]
-    partial class SistemaDeTarefasDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230626061512_VinculoTarefaUsuario2")]
+    partial class VinculoTarefaUsuario2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
