c0f26b1e86539d0df1738256e2f7d045fcf35ed2
Jeferson Macedo <jhef.salles@gmail.com>
2023-06-26 02:51:15 -0300

    Finalização do crud de usuários

---
+++ SistemaDeTarefas/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Repositorios;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
+namespace SistemaDeTarefas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<SistemaDeTarefasDBContext>(
+                options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"))
+            );
+
+            //builder.Services.AddEntityFrameworkSqlServer()
+            //    .AddDbContext<SistemaDeTarefasDBContext>(
+            //        options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase"))
+            //    );
+
+            builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
